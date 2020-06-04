9ef0d81bb2d252911ea8864aa5b7b4cc6b4a4769
Emil Atanasov <heitara@gmail.com>
2020-06-05 01:55:10 +0300

    fix typo

--- Tests/SwiftFMIRPCoreTests/XCTestManifests.swift
+++ Tests/SwiftFMIRPCoreTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(SwiftFMIRPGoreTests.allTests),
+        testCase(SwiftFMIRPCoreTests.allTests),
     ]
 }
 #endif
